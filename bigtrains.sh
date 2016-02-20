a86a476023c4d3fc22bdc6cdd2a4f8a9d51cd661
David Delgado <alohakealii@gmail.com>
2016-02-19 18:56:36 -0800

    Upload correct bigtrains.sh

--- bigtrains.sh
+++ bigtrains.sh
@@ -7,38 +7,45 @@ bin/rails g scaffold Topic title:text user:references
 bin/rails g scaffold Post body:text topic:references user:references
 bin/rake db:migrate
 
-# To set homepage: in bigtrains/config/routes.rb
-# uncomment line 9 and change "welcome" to "topics"
-# so line 9 will be: root 'topics#index'
+# Manual changes to routes.rb, models/topic.rb, views/topics/index.html.erb,
+# views/topics/show.html.erb, views/posts/index.html.erb,
+# views/posts/show.html.erb, views/layouts/application.html.erb
 
-# In bigtrains/app/views/topics/index.html.erb and
-# bshow.html.erb anywhere it says "topic.user"
-# add ".name" so it will be "topic.user.name"
+## To set homepage: in bigtrains/config/routes.rb
+## uncomment line 9 and change "welcome" to "topics"
+## so line 9 will be: root 'topics#index'
+##
+## In bigtrains/app/models/topic.rb add "has_many :posts"
+##
+## In bigtrains/app/views/topics/index.html.erb and
+## show.html.erb anywhere it says "topic.user"
+## add ".name" so it will be "topic.user.name"
+##
+## In bigtrains/app/views/posts/index.html.erb and
+## show.html.erb do the same thing for post.user
+## and add .title to post.topic
+##
+## To get the topics to display their respective posts, add
+## this code to bigtrains/app/views/topics/show.html.erb:
+##<p>
+##  <strong>Topic ID:</strong>
+##  <%= @topic.id %>
+##</p>
+##
+## <div id="posts">
+##   <p><strong>Posts:</strong></p>
+##   <% @topic.posts.each do |post| %>
+##     <div class="post">
+##       <p class="post-author"><%= post.user.name %> says:</p>
+##       <p class="post-body"><%= post.body %></p>
+##     </div>
+##   <% end %>
+##   <%= link_to 'New Post', new_post_path %>
+## </div>
+##
+## In views/layouts/application.html.erb
+## between body and yield, add:
+## <nav><%= link_to "Home", root_path %></nav>
+## <%= link_to "Show accounts", users_path %><br>
 
-# In bigtrains/app/views/posts/index.html.erb and
-# show.html.erb do the same thing for post.user
-# and add .title to post.topic
-
-# In bigtrains/app/models/topics.rb, add the line
-# has_many :posts to create a has_many relationship between
-# topics and posts
-
-# To get the topics to display their respective posts, add
-# this code to bigtrains/app/views/topics/show.html.erb:
-# <div id="posts">
-#   <p><strong>Posts:</strong></p>
-#   <% @topic.posts.each do |post| %>
-#     <div class="post">
-#       <p class="postbody"><%= post.body %></p>
-#       <p class="user"><%= post.user.name %></p>
-#     </div>
-#   <% end %>
-# </div>
-
-# To create a user go to http://localhost:3000/users
-
-# To create a topic go to http://localhost:3000/
-# and in the User field use the id (first user created is 1)
-
-# To create a Post go to http://localhost:3000/posts
-# and use ids for Topic and User fields
+# When creating topics and posts, use the IDs for users and topics
