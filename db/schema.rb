defa19358332828efa19ff815a1efcb9c42a71ee
David Delgado <alohakealii@gmail.com>
2016-03-28 09:39:32 -0700

    Add email and password for users with bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218225923) do
+ActiveRecord::Schema.define(version: 20160327181939) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 20160218225923) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
