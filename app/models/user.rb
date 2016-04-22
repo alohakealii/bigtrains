b215efb0f00284784bac3718900ae2500a1f4609
David Delgado <alohakealii@gmail.com>
2016-04-22 14:11:16 -0700

    Fix primary group

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
 	has_many :posts
+	belongs_to :primary_group, class_name: "Group", foreign_key: "primary_group"
 
 	has_secure_password
 end
